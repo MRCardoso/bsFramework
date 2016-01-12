b8324eae2195873dcc0d5620386b2d6281b5d351
MRCardoso <cardozinhos.mrc@gmail.com>
2016-01-12 00:50:45 -0200

    config

--- yii/helpers/preparing.php
+++ yii/helpers/preparing.php
@@ -43,7 +43,7 @@
         function formatDatabase($date,$moreDay=0)
         {
             $datebase = explode('/',$date);
-            return $datebase[2].'-'.$datebase[1].'-'.$datebase[0];
+            return $datebase[2].'-'.$datebase[1].'-'.($datebase[0]+$moreDay);
         }
     }
     if ( ! function_exists('t') )
