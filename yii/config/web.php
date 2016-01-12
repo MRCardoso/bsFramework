99ce039afd5384097039548a2b8438c21d3ac51c
MRCardoso <cardozinhos.mrc@gmail.com>
2016-01-12 00:54:24 -0200

    config

--- yii/config/web.php
+++ yii/config/web.php
@@ -15,9 +15,6 @@ $config = [
             'thousandSeparator' => '.',
             'decimalSeparator' => ',',
             'currencyCode' => 'R$',
-            'defaultTimeZone' => 'UTC',
-            'dateFormat' => 'php:d-m-Y',
-            'datetimeFormat'=>'php:d-M-Y H:i:s'
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
